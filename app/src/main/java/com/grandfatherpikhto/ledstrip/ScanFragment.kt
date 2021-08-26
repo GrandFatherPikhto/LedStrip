2ba6277b4ed822d0f539dca8df8050a600d4ec22
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-08-26 20:11:07 +0300

    Переименованы FirstFragment.kt в ScanFragment.kt, SecondFragment.kt в LedstripFragment.kt

--- app/src/main/java/com/grandfatherpikhto/ledstrip/ScanFragment.kt
+++ app/src/main/java/com/grandfatherpikhto/ledstrip/ScanFragment.kt
@@ -11,7 +11,7 @@ import com.grandfatherpikhto.ledstrip.databinding.FragmentFirstBinding
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class ScanFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
 
