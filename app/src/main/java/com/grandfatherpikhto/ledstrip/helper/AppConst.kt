6a98c2cff5802246fe42bf69fd48f921296a933a
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-10-13 22:53:34 +0300

    Константы приложения.

---
+++ app/src/main/java/com/grandfatherpikhto/ledstrip/helper/AppConst.kt
@@ -0,0 +1,7 @@
+package com.grandfatherpikhto.ledstrip.helper
+
+object AppConst {
+    const val PREFERENCES = "LedstripPreferences"
+    const val DEVICE_NAME = "DeviceName"
+    const val DEVICE_ADDRESS = "DeviceAddress"
+}
