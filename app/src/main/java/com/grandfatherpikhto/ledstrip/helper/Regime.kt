87167bd0e72ab4a3d0293d60b4a34da469503479
Grandfather Pikhto <grandfatherpikhto@gmail.com>
2021-11-21 14:47:05 +0300

    Режимы вынесены в отдельный класс. Добавлено преобразование в List строк с именами режимов для добавления в NumberPicker

---
+++ app/src/main/java/com/grandfatherpikhto/ledstrip/helper/Regime.kt
@@ -0,0 +1,30 @@
+package com.grandfatherpikhto.ledstrip.helper
+
+import com.grandfatherpikhto.ledstrip.service.BtLeService
+
+enum class Regime(val value: Int) {
+    Off(0x00),
+    Color(0x01),
+    Tag(0x02),
+    Water(0x03),
+    Tail(0x04),
+    Blink(0x05);
+
+    val enabled:Boolean get() {
+        if(this == Off) {
+            return false
+        }
+
+        return true
+    }
+
+    companion object {
+        private val VALUES = Regime.values()
+        fun getByValue(value: Int) = VALUES.firstOrNull { it.value == value }
+        fun toStringList(): List<String> {
+            return values().map {
+                it.toString()
+            }
+        }
+    }
+}
